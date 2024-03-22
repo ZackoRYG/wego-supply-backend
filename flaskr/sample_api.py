42155b24d54ab6a2a2cce7956b499712cb7780ed
Ethan Burd <54852139+burdbrains@users.noreply.github.com>
2024-03-21 20:01:50 -0500

    Backend setup.

---
+++ flaskr/sample_api.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import request
+
+@app.route("/call-backend", methods=['POST'])
+def sample_api():
+    print("backend hit")
+    request_body = request.get_json()
+    return 200, "200 OK!"
