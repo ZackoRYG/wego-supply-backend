24ed74d015f0c58a3cc24a170156f9a228fc405d
teweller <teweller@HAL>
2024-03-29 04:27:22 -0500

    First major restructure: Creation of folders api/model, api/route, api/service. vehicle_request.py renamed to vehicle.py

---
+++ api/route/vehicle.py
@@ -0,0 +1,37 @@
+import json
+from http import HTTPStatus
+from flask import Flask, request, jsonify, Blueprint, make_response
+from schema.db_initialization import db
+from schema.db_models import CounterTable
+from flask_cors import cross_origin, CORS
+from sqlalchemy.sql import func
+
+vehicle_api = Blueprint('vehicle', __name__)
+
+def add_counter():
+    db.session.add(CounterTable(count=1))
+    db.session.commit()
+
+def sum_counter():
+    # sums "count" field of all values
+    # in the database
+    return int(db.session.query(func.sum(CounterTable.count)).scalar())
+
+@vehicle_api.route("/vehicle-request", methods=['POST'])
+@cross_origin()
+def vehicle_request():
+    add_counter()
+    print("backend hit")
+    request_body = request.get_json
+    return make_response("200 OK!", HTTPStatus.OK.value)
+
+@vehicle_api.route("/vehicle-request-count", methods=['GET'])
+@cross_origin()
+def counter_return():
+    sum = sum_counter()
+    print(f"attempting to send GET: {sum}")
+    return jsonify(
+        {
+            "count_sum": sum
+        }
+    )
--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from config import DB_CONNECTION, PORT
 
 from schema.db_initialization import db
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{DB_CONNECTION}'
 db.init_app(app)
 
@@ -13,9 +13,22 @@ from schema.db_models import CounterTable
 with app.app_context():
     db.create_all()
 
-from api.sample_api import *
-from api.vehicle_request import *
-from api.react_request import *
+
+from api.route.vehicle import vehicle_api
+app.register_blueprint(vehicle_api, url_prefix = '/vehicle')
+
+from api.route.sample_api import *
+
+# from api.route.react_request import *
+
+
+'''
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(vehicle_api, url_prefix = '/vehicle')
+    return app
+'''
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=f'{PORT}') 
