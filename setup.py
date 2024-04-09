d9d67bfb755c61a88cdc39e198a3024acc5217d4
dresendiz <dresendi@stedwards.edu>
2024-04-09 13:51:33 -0500

    I change user.py, order.py, test_user.py, and test_object.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+#This is importing the setup tools from set up and the function of find_packages
+from setuptools import setup, find_packages
+
+#We us set up so our project "supply_backend" will know the relative files
+setup(name='supply_backend', version="1.0", packages = find_packages())
